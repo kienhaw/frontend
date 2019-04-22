52404242ccd637213e021890f8810930bf87e870
kienhaw <32449757+kienhaw@users.noreply.github.com>
2019-04-22 19:10:50 +0800

    updated default pagesize

--- src/app/components/issue/list/list.component.ts
+++ src/app/components/issue/list/list.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { MatTableDataSource, MatSort, MatPaginator, MatDialog, MatDialogConfig } from '@angular/material';
+import {
+  MatTableDataSource,
+  MatSort,
+  MatPaginator,
+  MatDialog,
+  MatDialogConfig
+} from '@angular/material';
 
 import { Issue } from '../../../issue.model';
 import { IssueService } from '../../../issue.service';
@@ -12,15 +18,24 @@ import { DeleteComponent } from '../delete/delete.component';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
   issues: Issue[];
-  displayedColumns: string[] = ['title', 'responsible', 'severity', 'status', 'actions'];
+  displayedColumns: string[] = [
+    'title',
+    'responsible',
+    'severity',
+    'status',
+    'actions'
+  ];
   listIssues: MatTableDataSource<any>;
   @ViewChild(MatSort) sort: MatSort; //import matsort function
   @ViewChild(MatPaginator) paginator: MatPaginator; //import matpaginator function
-  searchKey: String;
+  searchKey: String = '';
 
-  constructor(private issueService: IssueService, private router: Router, private dialog: MatDialog) { }
+  constructor(
+    private issueService: IssueService,
+    private router: Router,
+    private dialog: MatDialog
+  ) { }
 
   ngOnInit() {
     this.fetchIssues();
@@ -28,15 +43,18 @@ export class ListComponent implements OnInit {
 
   fetchIssues() {
     this.issueService.getIssues().subscribe((data: Issue[]) => {
-      this.issues = data; // without pagination
-      this.listIssues = new MatTableDataSource(data); //with pagination
+      this.issues = data["data"]; // without pagination
+      this.listIssues = new MatTableDataSource(data["data"]); //with pagination
       this.listIssues.sort = this.sort; //calling matsort to list issue
       this.listIssues.paginator = this.paginator; //calling matpaginator to list issue
+      // tslint:disable-next-line:no-shadowed-variable
       this.listIssues.filterPredicate = (data, filter) => {
         return this.displayedColumns.some(ele => {
-          return ele != 'actions' && data[ele].toLowerCase().indexOf(filter) != -1;
+          return (
+            ele !== 'actions' && data[ele].toLowerCase().indexOf(filter) !== -1
+          );
         });
-      }
+      };
     });
   }
 
@@ -53,8 +71,7 @@ export class ListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log('This dialog was closed');
       this.fetchIssues();
-    })
-
+    });
   }
 
   onSearchClear() {
@@ -65,5 +82,4 @@ export class ListComponent implements OnInit {
   applyFilter() {
     this.listIssues.filter = this.searchKey.trim().toLowerCase();
   }
-
 }
